08e26c84b1a81fb6b8cfaf5a589ec02cd95b0ef1
salty <136202579+simplysalt@users.noreply.github.com>
2026-05-08 11:45:16 -0700

    init code and links list

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const idk = "wow!";
+import redirects from './redirects.json';
+
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+    const path = url.pathname.replace(/^\/|\/$/g, ''); // Remove leading/trailing slashes
+
+    // Default redirect if the path is empty or not found
+    const target = redirects[path] || "https://salty.fyi";
+
+    return Response.redirect(target, 302);
+  },
+};
