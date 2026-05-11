3321cf152403bc1b457f8a3c8e7f1c166e191c88
salty <136202579+simplysalt@users.noreply.github.com>
2026-05-11 09:13:54 -0700

    wee woo eeweoweow

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ redirects.forEach(group => {
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    const path = url.pathname.replace(/^\/|\/$/g, '').toLowerCase; // Remove leading/trailing slashes
+    const path = url.pathname.replace(/^\/|\/$/g, '').toLowerCase(); // Remove leading/trailing slashes
 
-    // Default redirect if the path is empty or not found
-    const target = redirectMap.get(path) || "https://find.salty.fyi";
+    const target = redirectMap.get(path);
 
-    return Response.redirect(target, 302);
+    if (target) {
+      return Response.redirect(target, 302);
+    }
+
+    // Fallback to your main site if no alias is found
+    return Response.redirect("https://salty.fyi", 302);
   },
 };
