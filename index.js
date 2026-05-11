079b5f2560e3b754cb78d85769c7550993c9c5bb
salty <136202579+simplysalt@users.noreply.github.com>
2026-05-11 08:53:38 -0700

    aa

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import redirects from './redirects.json';
 
+const redirectMap = new Map();
+redirects.forEach(group => {
+  group.urls.forEach(alias => {
+    redirectMap.set(alias.toLowerCase(), group.target);
+  });
+});
+
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    const path = url.pathname.replace(/^\/|\/$/g, ''); // Remove leading/trailing slashes
+    const path = url.pathname.replace(/^\/|\/$/g, '').toLowerCase; // Remove leading/trailing slashes
 
     // Default redirect if the path is empty or not found
-    const target = redirects[path] || "https://salty.fyi";
+    const target = redirectMap.get(path) || "https://find.salty.fyi";
 
     return Response.redirect(target, 302);
   },
